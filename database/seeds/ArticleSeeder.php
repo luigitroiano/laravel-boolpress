abdcdb0bf5d7be16ff9185f7c9fb873a6f053666
LuigiTroiano <troiano.luigi8@gmail.com>
2021-02-23 15:41:53 +0100

    Creato e popolato db

---
+++ database/seeds/ArticleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Article;
+use Faker\Generator as Faker;
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 30; $i++) { 
+            $newArticle = new Article();
+            $newArticle->title = $faker->sentence(3);
+            $newArticle->subtitle = $faker->sentence(10);
+            $newArticle->body = $faker->paragraph();
+            $newArticle->author = $faker->name();
+            $newArticle->reading_time = $faker->randomDigitNotNull();
+            $newArticle->save();
+        }
+    }
+}
